91fb33eeee67c344f31deb68426dcac7d307a081
Cirga-24 <felipegarxiapaes@gmail.com>
2026-04-02 01:37:49 -0500

    VerficacionDeLoginPrueba

--- assets/js/loginScripts.js
+++ assets/js/loginScripts.js
@@ -23,3 +23,27 @@ document.getElementById('btn_cancelarforgot').addEventListener('click', function
     document.querySelector('.contenedor_forgotpassword').style.display = 'none';
     document.querySelector('.contenedor_login').style.display = 'grid';
 });
+
+//Boton para enviar formulario de ingreso
+document.getElementById('btn_ingresar').addEventListener('click', function(e) {
+    e.preventDefault();
+    var username = document.getElementById('usuario').value.trim();
+    var password = document.getElementById('password').value.trim();
+
+    if(username == "" || password == "") 
+    {
+        document.getElementById('alertText').textContent = 'Por favor, complete todos los campos.';
+        setTimeout(() => {
+            alertText.textContent = '';
+        }, 3000);
+        return;
+    };
+
+    if(username === 'adminFull' && password === 'FullAdmin') {
+        window.location.href = 'assets/pages/homeAdmin.html';
+    } else if(username === 'worker' && password === 'FullWorker') {
+        window.location.href = 'assets/pages/homeWorker.html';
+    } else {
+        document.getElementById('alertText').textContent = 'Usuario o contraseña incorrectos.';
+    }
+});
