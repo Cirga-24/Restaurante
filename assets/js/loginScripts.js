c2f879e55bb5ef3901e6c71425ecd0e4363e0f1e
Cirga-24 <felipegarxiapaes@gmail.com>
2026-04-02 00:06:15 -0500

    ConfiguracionBotonesLoginYEstilos

--- assets/js/loginScripts.js
+++ assets/js/loginScripts.js
@@ -1,8 +1,25 @@
-// Botones de mostrar/ocultar formulario
+// Boton para mostrar formulario de ingreso
 document.getElementById('btn_login').addEventListener('click', function() {
-    document.querySelector('.contenedor_login').classList.add('active');
+    document.querySelector('.contenedor_todo').classList.add('active');
+    document.querySelector('.contenedor_login').style.display = 'grid';
 });
 
+//Boton para cancelar ingreso
 document.getElementById('btn_cancelar').addEventListener('click', function() {
-    document.querySelector('.contenedor_login').classList.remove('active');
+    document.querySelector('.contenedor_todo').classList.remove('active');
+    document.querySelector('.contenedor_login').style.display = 'none';
+});
+
+//Boton de "Olvidó su contraseña"
+document.getElementById('btn_forgotpassword').addEventListener('click', function() {
+    document.querySelector('.contenedor_forgotpassword').classList.add('active');
+    document.querySelector('.contenedor_forgotpassword').style.display = 'grid';
+    document.querySelector('.contenedor_login').style.display = 'none';
+});
+
+//Boton para cancelar "Olvidó su contraseña"
+document.getElementById('btn_cancelarforgot').addEventListener('click', function() {
+    document.querySelector('.contenedor_forgotpassword').classList.remove('active');
+    document.querySelector('.contenedor_forgotpassword').style.display = 'none';
+    document.querySelector('.contenedor_login').style.display = 'grid';
 });
